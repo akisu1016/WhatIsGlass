3d86c64dd5516924a05752d711f320e376dbd61b
Akisu <akisu1016@yahoo.co.jp>
2021-06-23 14:16:38 +0900

    エラーハンドリング処理を追加

--- app/api/views/question.py
+++ app/api/views/question.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, make_response, jsonify, session
+from flask import Blueprint, request, make_response, jsonify, session, abort
 from api.models import Index, IndexSchema, Answer, AnswerSchema
 import json
 
@@ -6,20 +6,27 @@ import json
 question_router = Blueprint("question_router", __name__)
 
 
+@question_router.errorhandler(400)
+def error_handler(err):
+    res = jsonify({"error": {"message": err.description["message"]}, "code": err.code})
+    return res, err.code
+
+
 @question_router.route("/question", methods=["GET"])
 def getIndexList():
 
-    contents = request.args
-
-    request_dict = {
-        "sort": contents.get("sort"),
-        "language_id": contents.get("language_id"),
-        "include_no_answer": contents.get("include_no_answer"),
-        "keyword": contents.get("keyword"),
-    }
-
-    indices = Index.getIndexList(request_dict)
-    index_schema = IndexSchema(many=True)
+    try:
+        contents = request.args
+        request_dict = {
+            "sort": contents.get("sort"),
+            "language_id": contents.get("language_id"),
+            "include_no_answer": contents.get("include_no_answer"),
+            "keyword": contents.get("keyword"),
+        }
+        indices = Index.getIndexList(request_dict)
+        index_schema = IndexSchema(many=True)
+    except ValueError:
+        print(ValueError)
 
     return make_response(jsonify({"code": 200, "indices": index_schema.dump(indices)}))
 
@@ -31,9 +38,15 @@ def registIndex():
     jsonData = json.dumps(request.json)
     indexData = json.loads(jsonData)
 
+    if not "questioner" in indexData:
+        abort(401, {"message": "questioner is a required!!"})
+
     # TODO : JsonDataにquestionerのIDを追加する
 
-    index = Index.registIndex(indexData)
-    index_schema = IndexSchema(many=True)
+    try:
+        index = Index.registIndex(indexData)
+        index_schema = IndexSchema(many=True)
+    except ValueError:
+        print(ValueError)
 
     return make_response(jsonify({"code": 201, "index": index_schema.dump(index)}))
