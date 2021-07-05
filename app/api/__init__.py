cc06abbdf0faae21b7d8e91e1ffbbeab02c2c2f5
松下玲司 <matsushitareiji@matsushitareijinoMacBook-Pro.local>
2021-07-05 13:56:56 +0900

    回答投稿・回答回覧APIの作成

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, make_response, jsonify
 from .views.user import user_router
 from .views.question import question_router
+from .views.answer import answer_router
 from flask_cors import CORS
 from api.database import db
 import config
@@ -20,6 +21,8 @@ def create_app():
 
     app.register_blueprint(user_router, url_prefix="/api")
     app.register_blueprint(question_router, url_prefix="/api")
+    # answerのルーティング設定
+    app.register_blueprint(answer_router, url_prefix="/api")
 
     return app
 
--- app/api/models/answer.py
+++ app/api/models/answer.py
@@ -1,5 +1,6 @@
-import re
 from api.database import db, ma
+from sqlalchemy import create_engine, text
+from .example import Example
 import datetime
 
 
@@ -16,17 +17,56 @@ class Answer(db.Model):
     date = db.Column(db.TIMESTAMP, nullable=True)
 
     def __repr__(self):
-        return "<Answer %r>" % self.name
+        return "<Answer %r>" % self.definition
 
-    def getAnswerList():
+    def getAnswerList(request_index_id):
 
         # select * from users
-        answer_list = db.session.query(Answer).all()
+        answer_list = (
+            db.session.query(Answer)
+            .filter(request_index_id["index_id"] == Answer.index_id)
+            .all()
+        )
 
-        if answer_list == None:
+        if answer_list is None:
             return []
         else:
             return answer_list
+
+    def registAnswer(answer):
+
+        record = Answer(
+            id=0,
+            user_id=1,
+            informative_count=0,
+            index_id=answer["index_id"],
+            definition=answer["definition"],
+            origin=answer["origin"],
+            note=answer["note"],
+            date=datetime.datetime.now(),
+        )
+
+        # insert into answers(id, user_id, informative_count...) values(...)
+        db.session.add(record)
+        db.session.flush()
+        db.session.commit()
+
+        # exampleの登録処理(example.py作成時に記入予定)
+        if not answer["example"]:
+            return []
+        else:
+            for examplelist in answer["example"]:
+                answer_query = db.session.execute(
+                    "SELECT id from answers WHERE id = last_insert_id();"
+                )
+
+                sql = (
+                    "INSERT INTO example_answer(example_sentence, answer_id) VALUES('%s', '%d)"
+                    % (examplelist, answer_query[0][0])
+                )
+                db.session.execute(sql)
+
+        return answer
 
 
 class AnswerSchema(ma.SQLAlchemyAutoSchema):
